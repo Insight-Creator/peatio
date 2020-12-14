e5ed6164efa556f34b412de737089bc3a707ee7c
Maksym Naichuk <mnaichuk@heliostech.fr>
2020-12-14 21:55:46 +0200

    Fix: Update migration for currency field

--- db/migrate/20201206205429_change_currency_price.rb
+++ db/migrate/20201206205429_change_currency_price.rb
@@ -1,11 +1,17 @@
 class ChangeCurrencyPrice < ActiveRecord::Migration[5.2]
   def up
 <<<<<<< HEAD
+<<<<<<< HEAD
     Currency.find_each do |c|
       c.update(price: 1) if c.price.blank?
     end
 =======
 >>>>>>> 9b73ca2c (Update currency price field)
+=======
+    Currency.find_each do |c|
+      c.update(price: 1) if c.price.blank?
+    end
+>>>>>>> 7dbee855 (Fix: Update migration for currency field)
     change_column :currencies, :price, :decimal, precision: 32, scale: 16, null: false, default: 1.0
   end
 
